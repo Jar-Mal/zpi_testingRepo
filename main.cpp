9ac979a24a93b8097403ace559d10796d1591003
Breanss <100619723+Breanss@users.noreply.github.com>
2022-04-04 10:07:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main() {
     std::cout << "Maleszyk" << std::endl;
     std::cout << "Chromczak" << std::endl;
     std::cout << "Kopania" << std::endl;
+    std::cout << "Lukasiewicz" << std::endl;
     return 0;
 }
